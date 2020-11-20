7ce8acef5623affb012c36f139c1d1e46dda7a2e
Nu Maniphanh <maniphan@gmail.com>
2020-11-19 22:41:59 -0600

    Move component out from hooks

--- pages/clients/login/loginPage.js
+++ pages/clients/login/loginPage.js
@@ -4,28 +4,14 @@ import {ScrollView, View, Text, Button} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 import {LOGIN_SUCCESS, LOGIN_FAILED, LOG_OFF} from './actionTypes';
 
-const LoginPage = () => {
-  const currentUser = useSelector((state) => state.login.user);
-  const loginStatus = useSelector((state) => state.login.status);
-  const dispatch = useDispatch();
+const LoginView = (props) => {
+  const {signIn, signOut, user, status} = props;
 
-  const authenticator = auth();
-
-  const signIn = () => {
-    authenticator
-      .signInWithEmailAndPassword('nu.maniphanh@gmail.com', 'password')
-      .then((response) => dispatch({type: LOGIN_SUCCESS, user: response.user}))
-      .catch((error) => dispatch({type: LOGIN_FAILED, error}));
-  };
-
-  const signOut = () =>
-    authenticator.signOut().then(() => dispatch({type: LOG_OFF}));
-
-  if (loginStatus === LOGIN_SUCCESS) {
+  if (status === LOGIN_SUCCESS) {
     return (
       <ScrollView>
         <View>
-          <Text>Hello user! {currentUser.email}</Text>
+          <Text>Hello user! {user.email}</Text>
         </View>
         <View>
           <Button onPress={signOut} title="Sign out" color="#841584" />
@@ -44,6 +30,33 @@ const LoginPage = () => {
       </View>
     </ScrollView>
   );
+};
+
+const LoginPage = () => {
+  const user = useSelector((state) => state.login.user);
+  const status = useSelector((state) => state.login.status);
+  const dispatch = useDispatch();
+
+  const authenticator = auth();
+
+  const signIn = () => {
+    authenticator
+      .signInWithEmailAndPassword('nu.maniphanh@gmail.com', 'password')
+      .then((response) => dispatch({type: LOGIN_SUCCESS, user: response.user}))
+      .catch((error) => dispatch({type: LOGIN_FAILED, error}));
+  };
+
+  const signOut = () =>
+    authenticator.signOut().then(() => dispatch({type: LOG_OFF}));
+
+  const props = {
+    signOut,
+    signIn,
+    user,
+    status,
+  };
+
+  return <LoginView {...props} />;
 };
 
 export default LoginPage;
